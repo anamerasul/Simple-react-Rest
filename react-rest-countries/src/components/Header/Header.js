574b386e1fa02b2766fa4e5b295fd3ee173ff874
anamerasul <anamerasul@gmail.com>
2022-03-18 15:22:09 +0600

    recap use bootstrap in React

--- react-rest-countries/src/components/Header/Header.js
+++ react-rest-countries/src/components/Header/Header.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './Header.css'
 const Header = () => {
     return (
-        <div>
+        <div className='bg-primary'>
             <h1> Welcome to my Countries</h1>
-            <nav>
-                <a href="/home">Home</a>
+            <nav className='text-white'> 
+                <a  href="/home">Home</a>
                 <a href="/about">About</a>
                 <a href="/country">Country</a>
             </nav>
